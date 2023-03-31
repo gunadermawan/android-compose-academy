5bae3992e6c61a9f4e802bbe8a9a5cdfcb0543c9
gunadermawan <gunadermawan.official@gmail.com>
2023-04-01 06:49:26 +0700

    create two way converter with state hosting

--- app/src/main/java/com/gunder/compose/converter/MainActivity.kt
+++ app/src/main/java/com/gunder/compose/converter/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
                     Column {
                         StatefulTemperatureInput()
                         ConverterApp()
+                        TwoWayConverterApp()
                     }
                 }
             }
@@ -102,14 +103,71 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun convertToFahrenheit(celsius: String) =
-        celsius.toDoubleOrNull()?.let {
-            (it * 9 / 5) + 32
-        }.toString()
+        celsius.toDoubleOrNull()?.let { (it * 9 / 5) + 32 }.toString()
+
+    private fun convertToCelsius(fahrenheit: String) =
+        fahrenheit.toDoubleOrNull()?.let { (it - 32) * 5 / 9 }.toString()
+
+    enum class Scale(val scaleName: String) {
+        CELSIUS("Celsius"),
+        FAHRENHEIT("Fahrenheit")
+    }
+
+    @Composable
+    fun GeneralTemperatureInput(
+        scale: Scale,
+        input: String,
+        onValueChange: (String) -> Unit,
+        modifier: Modifier = Modifier,
+    ) {
+        Column(modifier) {
+            OutlinedTextField(value = input, onValueChange = onValueChange, label = {
+                Text(
+                    stringResource(R.string.enter_temperature, scale.scaleName)
+                )
+            }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        }
+    }
+
+    @Composable
+    fun TwoWayConverterApp(modifier: Modifier = Modifier) {
+        var celsius by remember {
+            mutableStateOf("")
+        }
+        var fahrenheit by remember {
+            mutableStateOf("")
+        }
+        Column(modifier.padding(16.dp)) {
+            Text(
+                text = stringResource(R.string.two_way_converter),
+                style = MaterialTheme.typography.h5
+            )
+            GeneralTemperatureInput(
+                scale = Scale.CELSIUS,
+                input = celsius,
+                onValueChange = {
+                    celsius = it
+                    fahrenheit = convertToFahrenheit(it)
+                })
+
+            GeneralTemperatureInput(
+                scale = Scale.FAHRENHEIT,
+                input = fahrenheit,
+                onValueChange = {
+                    fahrenheit = it
+                    celsius = convertToCelsius(it)
+                })
+        }
+    }
 
 
     @Preview(showBackground = true)
     @Composable
-    fun StatefulTemperatureInputPreview() {
-        StatefulTemperatureInput()
+    fun DefaultPreview() {
+        Column {
+            StatefulTemperatureInput()
+            ConverterApp()
+            TwoWayConverterApp()
+        }
     }
 }
