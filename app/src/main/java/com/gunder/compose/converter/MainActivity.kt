2e892e70260eeee9c1daef09726039dc970d4262
gunadermawan <gunadermawan.official@gmail.com>
2023-04-01 06:15:55 +0700

    create state hosting and stateless compose fun

--- app/src/main/java/com/gunder/compose/converter/MainActivity.kt
+++ app/src/main/java/com/gunder/compose/converter/MainActivity.kt
@@ -29,7 +29,10 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    StatefulTemperatureInput()
+                    Column {
+                        StatefulTemperatureInput()
+                        ConverterApp()
+                    }
                 }
             }
         }
@@ -59,6 +62,43 @@ class MainActivity : AppCompatActivity() {
                 })
             Text(stringResource(R.string.temperature_fahrenheit, output))
         }
+    }
+
+    // stateless compose func
+    @Composable
+    fun StatelessTemperatureInput(
+        input: String,
+        output: String,
+        onValueChange: (String) -> Unit,
+        modifier: Modifier = Modifier,
+    ) {
+        Column(modifier.padding(16.dp)) {
+            Text(
+                text = stringResource(R.string.stateless_converter),
+                style = MaterialTheme.typography.h5
+            )
+            OutlinedTextField(
+                value = input,
+                label = { Text(stringResource(R.string.enter_celsius)) },
+                onValueChange = onValueChange,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Text(stringResource(R.string.temperature_fahrenheit, output))
+        }
+    }
+
+    @Composable
+    private fun ConverterApp(modifier: Modifier = Modifier) {
+        var input by remember {
+            mutableStateOf("")
+        }
+        var output by remember {
+            mutableStateOf("")
+        }
+        StatelessTemperatureInput(input = input, output = output, onValueChange = {
+            input = it
+            output = convertToFahrenheit(it)
+        })
     }
 
     private fun convertToFahrenheit(celsius: String) =
