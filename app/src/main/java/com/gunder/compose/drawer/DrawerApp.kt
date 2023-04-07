6de50dc49afcf733d74416cdc1497fc7a652f3da
gunadermawan <gunadermawan.official@gmail.com>
2023-04-07 21:52:18 +0700

    update dependency and create icon in DrawerApp

--- app/src/main/java/com/gunder/compose/drawer/DrawerApp.kt
+++ app/src/main/java/com/gunder/compose/drawer/DrawerApp.kt
@@ -1,16 +1,24 @@
 package com.gunder.compose.drawer
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gunder.compose.R
 import com.gunder.compose.ui.theme.ComposeTheme
 import kotlinx.coroutines.launch
@@ -29,7 +37,9 @@ fun DrawerApp() {
                 }
             })
         },
-        drawerContent = { Text(stringResource(R.string.hello_from_nav_drawer)) },
+        drawerContent = {
+            DrawerContent(onItemSelected = {})
+        },
         drawerGesturesEnabled = scaffoldState.drawerState.isOpen
     ) { paddingValues ->
         Box(
@@ -55,6 +65,41 @@ fun MyTopBar(onMenuClick: () -> Unit) {
         },
         title = { Text(stringResource(R.string.app_name)) }
     )
+}
+
+data class MenuItem(val title: String, val icon: ImageVector)
+
+@Composable
+fun DrawerContent(modifier: Modifier = Modifier, onItemSelected: (title: String) -> Unit) {
+    val items = listOf(
+        MenuItem(title = stringResource(R.string.home), icon = Icons.Default.Home),
+        MenuItem(title = stringResource(R.string.favourite), icon = Icons.Default.Favorite),
+        MenuItem(title = stringResource(R.string.profile), icon = Icons.Default.AccountCircle),
+    )
+    Column(modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .height(190.dp)
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.primary)
+        )
+        for (item in items) {
+            Row(modifier = Modifier
+                .clickable { onItemSelected(item.title) }
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = item.title,
+                    tint = Color.DarkGray
+                )
+                Spacer(modifier = Modifier.width(32.dp))
+                Text(text = item.title, style = MaterialTheme.typography.subtitle2)
+            }
+        }
+        Divider()
+    }
 }
 
 @Preview
